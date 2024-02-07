b772dd5b41df4fd28c97897944049caea09169e9
vikash <vvikashrao203@gmail.com>
2024-02-07 20:21:30 +0530

    updated

--- models/url.js
+++ models/url.js
@@ -16,6 +16,6 @@ const urlSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 
-const URL=mongoose.model('url',urlSchema);
+const URL = mongoose.model('url', urlSchema);
 
-module.exports=URL;
+module.exports = URL
