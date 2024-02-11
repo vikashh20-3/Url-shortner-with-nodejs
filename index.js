f6c183aac4a6f771898c8f47da59f9d81f460c6b
vikash <vvikashrao203@gmail.com>
2024-02-11 15:41:46 +0530

    updated

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 const express = require("express");
-
-const { connectToMongoDB } = require('./connect')
-const urlRoute = require('./routes/url')
+const { connectToMongoDB } = require('./connect');
+const urlRoute = require('./routes/url');
 const app = express();
-const URL = require('./models/url')
+const URL = require('./models/url');
 
-// this middleware is used to parse the body
-app.use(express.json())
+// Middleware to parse the request body
+app.use(express.json());
 
+// Route for handling shortened URLs
 app.use("/url", urlRoute);
 
+// Route for redirecting based on shortID
 app.get('/:shortID', async (req, res) => {
+
     const shortID = req.params.shortID;
-    const entry = await URL.findOneAndUpdate({
-        shortID
-    }, {
-        $push: {
-            visitHistory: Date.now()
+    console.log("Requested shortID:", shortID);
+    // Find the URL entry with the provided shortID
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortID,
+        },
+        {
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now(),
+                },
+            },
         }
-
-    })
-    res.redirect(entry.redirectURL)
-
+    );
+    res.redirect(entry.redirectURL);
 });
 
+// Connect to MongoDB
 connectToMongoDB('mongodb://localhost:27017/short-url')
-    .then(() => console.log('mongo db connected'))
-const PORT = 8001;
-app.listen(PORT, () => console.log(`Server Started at ${PORT}`));
+    .then(() => console.log('MongoDB connected'))
+    .catch(error => console.error('MongoDB connection error:', error));
+
+// Start the server
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Server started at ${PORT}`));
