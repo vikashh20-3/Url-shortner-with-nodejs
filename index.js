0d2c29cb4cb6971aa96b4fc0c5f696b78f1cbd03
vikash <vvikashrao203@gmail.com>
2024-02-11 08:34:31 +0530

    mongo db connected

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+
+const { connectToMongoDB } = require('./connect')
 const urlRoute = require('./routes/url')
 const app = express();
 app.use("/url", urlRoute);
