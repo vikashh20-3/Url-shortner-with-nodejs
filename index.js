d0175e1207fefee68f821958eff794c859d8c3f8
vikash <vvikashrao203@gmail.com>
2024-02-11 08:35:53 +0530

    mongo db path given

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require("express");
 const { connectToMongoDB } = require('./connect')
 const urlRoute = require('./routes/url')
 const app = express();
+
+
 app.use("/url", urlRoute);
+
+connectToMongoDB('mongodb://localhost:27017/short-url')
+    .then(() => console.log('mongo db connected'))
 const PORT = 8001;
 app.listen(PORT, () => console.log(`Server Started at ${PORT}`));
