816c19e81e0ea19525bce7d98df28f75642f7656
vikash <vvikashrao203@gmail.com>
2024-02-11 09:16:42 +0530

    redirect the main url with short url

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const express = require("express");
 const { connectToMongoDB } = require('./connect')
 const urlRoute = require('./routes/url')
 const app = express();
+const URL = require('./models/url')
 
+// this middleware is used to parse the body
 app.use(express.json())
 
 app.use("/url", urlRoute);
 
-// this middleware is used to parse the body
+app.get('/:shortID', async (req, res) => {
+    const shortID = req.params.shortID;
+    const entry = await URL.findOneAndUpdate({
+        shortID
+    }, {
+        $push: {
+            visitHistory: Date.now()
+        }
+
+    })
+    res.redirect(entry.redirectURL)
+
+});
 
 connectToMongoDB('mongodb://localhost:27017/short-url')
     .then(() => console.log('mongo db connected'))
